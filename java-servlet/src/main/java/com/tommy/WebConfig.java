380e2c0643e465a36c861dbad839ada527d5e1ef
임한결 <dlagksruf19@naver.com>
2021-02-23 15:16:12 +0900

    [Spring MVC 설정] @EnableWebMvc

--- java-servlet/src/main/java/com/tommy/WebConfig.java
+++ java-servlet/src/main/java/com/tommy/WebConfig.java
@@ -2,14 +2,24 @@ package com.tommy;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
- * Spring Web Mvc
+ * Spring Web Mvc, Spring MVC 설정
  * Java Servlet Application
- * Spring Web Mvc 연동
+ * Spring Web Mvc 연동, @EnableWebMvc 설정
  */
-@Configuration
 // (useDefaultFilters = false, includeFilters = @ComponentScan.Filter(Controller.class))
+@Configuration
 @ComponentScan
-public class WebConfig {
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp("/WEB-INF/", ".jsp");
+    }
+    
 }
