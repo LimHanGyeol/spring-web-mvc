c7470384e2f5370ce882129da4ccb7464670803f
임한결 <dlagksruf19@naver.com>
2021-02-24 18:38:58 +0900

    [Spring MVC 설정] Resource Handler

--- src/main/java/com/example/springmvc/config/WebConfig.java
+++ src/main/java/com/example/springmvc/config/WebConfig.java
@@ -1,11 +1,15 @@
-package com.example.springmvc.sample;
+package com.example.springmvc.config;
 
 import com.example.springmvc.config.AnotherInterceptor;
 import com.example.springmvc.config.GreetingInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Spring Web Mvc
@@ -29,5 +33,15 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(new AnotherInterceptor())
                 .addPathPatterns("/hello/**")
                 .order(-1);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/mobile/**")
+                .addResourceLocations("classpath:/mobile/")
+                .setCacheControl(CacheControl.maxAge(10, TimeUnit.MINUTES))
+                .resourceChain(true); // 캐쉬를 사용할지 말지. 운영중이라면 true, 개발중이라면 false
+                // .addResolver() // 어떤 요청에 해당하는 Resource 탐색
+                // .addTransformer() // 응답으로 내보낼 리소스를 변경하는 방법
     }
 }
--- src/test/java/com/example/springmvc/sample/SampleControllerTest.java
+++ src/test/java/com/example/springmvc/sample/SampleControllerTest.java
@@ -1,15 +1,16 @@
 package com.example.springmvc.sample;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 /**
  * Spring Web Mvc
@@ -28,7 +29,10 @@ class SampleControllerTest {
 
     @Test
     void hello() throws Exception {
-        mockMvc.perform(get("/hello/hangyeol"))
+        Person person = new Person("hangyeol");
+        Person savedPerson = personRepository.save(person);
+
+        mockMvc.perform(get("/hello/" + savedPerson.getId().toString()))
                 .andDo(print())
                 .andExpect(content().string("hello hangyeol"));
     }
@@ -43,6 +47,23 @@ class SampleControllerTest {
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().string("hello hangyeol"));
-
     }
+
+    @Test
+    void helloStatic() throws Exception {
+        mockMvc.perform(get("/index.html"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(Matchers.containsString("hello index")));
+    }
+
+    @Test
+    void helloMobileStatic() throws Exception {
+        mockMvc.perform(get("/mobile/index.html"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(Matchers.containsString("hello mobile")))
+                .andExpect(header().exists(HttpHeaders.CACHE_CONTROL));
+    }
+
 }
