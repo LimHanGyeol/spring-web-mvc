20a8a675ead56d4c179440a60dbcf3ad9421458d
임한결 <dlagksruf19@naver.com>
2021-03-01 11:54:13 +0900

    [Spring MVC 활용] Handler Method Request Parameters

--- src/main/java/com/example/springmvc/event/controller/EventApiController.java
+++ src/main/java/com/example/springmvc/event/controller/EventApiController.java
@@ -2,10 +2,7 @@ package com.example.springmvc.event.controller;
 
 import com.example.springmvc.event.domain.Event;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.MatrixVariable;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Spring Web MVC 활용
@@ -15,12 +12,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class EventApiController {
 
+    //public Event getEvent(@PathVariable long id, @MatrixVariable String name) {
     @GetMapping("/events/{id}")
-    public Event getEvent(@PathVariable long id, @MatrixVariable String name) {
+    public Event getEvent(@PathVariable long id) {
+        return Event.builder()
+                .id(id)
+                .name("springboot")
+                .limitOfEnrollment(5)
+                .build();
+    }
+
+    /**
+     * Request Parameter 는 Map<String, String> 으로 모든 파라미터를 받아올 수 있다.
+     */
+    @PostMapping("/events/{id}")
+    public Event getEventByName(@PathVariable long id,
+                                @RequestParam(required = false, defaultValue = "spring") String name,
+                                @RequestParam int limit) {
         return Event.builder()
                 .id(id)
                 .name(name)
-                .limitOfEnrollment(5)
+                .limitOfEnrollment(limit)
                 .build();
     }
 
--- src/test/java/com/example/springmvc/event/controller/EventApiControllerTest.java
+++ src/test/java/com/example/springmvc/event/controller/EventApiControllerTest.java
@@ -1,12 +1,12 @@
 package com.example.springmvc.event.controller;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -25,13 +25,25 @@ class EventApiControllerTest {
                 .andExpect(jsonPath("$.id").value(1));
     }
 
+//    @Test
+//    @DisplayName("Matrix Variable 을 사용한 요청")
+//    void getEventByName() throws Exception {
+//        mockMvc.perform(get("/events/1;name=spring"))
+//                .andDo(print())
+//                .andExpect(status().isOk())
+//                .andExpect(jsonPath("$.id").value(1))
+//                .andExpect(jsonPath("$.name").value("spring"));
+//    }
+
     @Test
-    @DisplayName("Matrix Variable 을 사용한 요청")
-    void getEventByName() throws Exception {
-        mockMvc.perform(get("/events/1;name=spring"))
+    void getEventPost() throws Exception {
+        mockMvc.perform(post("/events/1")
+                .param("name", "springboot")
+                .param("limit", "3"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id").value(1))
-                .andExpect(jsonPath("$.name").value("spring"));
+                .andExpect(jsonPath("$.name").value("springboot"))
+                .andExpect(jsonPath("$.limitOfEnrollment").value("3"));
     }
 }
