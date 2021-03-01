bf2c11a7b114909218345bd15b67eae3029b08f8
임한결 <dlagksruf19@naver.com>
2021-03-01 21:45:15 +0900

    [Spring MVC 활용] Handler Method RequestBody & HttpEntity

---
+++ src/main/java/com/example/springmvc/event/controller/EventRequestBodyController.java
@@ -0,0 +1,41 @@
+package com.example.springmvc.event.controller;
+
+import com.example.springmvc.event.domain.Event;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+/**
+ * Spring Web MVC 활용
+ * RequestBody, HttpEntity
+ * RequestBody 확인용 컨트롤러. 예제 이름 부족으로 RequestBody ClassName 사용
+ * 원하는 HttpMessageConverter 가 없을 경우.. WebConfig - ExtendsMessageConverters 를 추가하여 사용할 수 있다.
+ * configureMessageConverters 도 있지만 이 메서드를 사용하면 기존에 등록되어 있던 HttpMessageConverters 가 다 사용이 불가능해진다.
+ *
+ */
+@RestController
+@RequestMapping("/api/events")
+public class EventRequestBodyController {
+
+    /**
+     * HttpEntity<T> 를 사용하면 RequestBody 어노테이션을 사용하지 않고도,
+     * HttpMessageConverter 가 Body 를 Conversion 해줄 것이다.
+     * RequestBody 어노테이션으로 가져온 타입과 다르게 HttpEntity 는 Header 도 가져올 수 있다.
+     * HttpEntity<Event> request
+     * MediaType contentType = request.getHeaders().getContentType();
+     * return request.getBody();
+     */
+    @PostMapping("")
+    public Event create(@Valid @RequestBody Event event, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            bindingResult.getAllErrors().forEach(System.out::println);
+        }
+        // db save
+        return event;
+    }
+
+}
---
+++ src/test/java/com/example/springmvc/event/controller/EventRequestBodyControllerTest.java
@@ -0,0 +1,45 @@
+package com.example.springmvc.event.controller;
+
+import com.example.springmvc.event.domain.Event;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class EventRequestBodyControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    @DisplayName("RequestBody Test")
+    void create() throws Exception {
+        // given
+        Event event = new Event(1L, "spring", 5);
+        String requestBody = objectMapper.writeValueAsString(event);
+
+        // when, then
+        mockMvc.perform(post("/api/events")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value("1"))
+                .andExpect(jsonPath("$.name").value("spring"))
+                .andExpect(jsonPath("$.limitOfEnrollment").value("5"));
+    }
+}
